bd38a401a5234bff935c2e96872990b687bcd4ee
Rory Henderson <rhenderson@rim.net>
2016-06-30 15:54:42 -0400

    start.

--- src/Logalyzer/GUI.java
+++ src/Logalyzer/GUI.java
@@ -1,9 +1,43 @@
 package Logalyzer;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by rhenderson on 6/28/2016.
  */
 public class GUI extends JFrame {
+    private JTextArea displayConsole = new JTextArea(40, 85);
+    private JScrollPane consolePane = new JScrollPane(displayConsole);
+
+    public JTextArea getDisplayConsole() {
+        return displayConsole;
+    }
+
+    public void setDisplayConsole(JTextArea displayConsole) {
+        this.displayConsole = displayConsole;
+    }
+
+    private JTextField httpsURLInput = new JTextField(50);
+    private JButton executeButton = new JButton();
+
+    public GUI(String titleOfWindow, int width, int height){
+        setSize(width, height);
+        setTitle(titleOfWindow);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        displayConsole.setEditable(false);
+        httpsURLInput.setText("59BF2C55C1440A8FEEEBB1538D9D6064:/bbIVR");
+        executeButton.setText("Pull log of Call");
+        displayConsole.setLineWrap(true);
+        setVisible(true);
+        add(httpsURLInput);
+        add(consolePane);
+        add(executeButton);
+    }
+
+    public void reloadGUI(){
+
+    }
+
 }
--- src/Logalyzer/Logalyzer.java
+++ src/Logalyzer/Logalyzer.java
@@ -1,7 +1,60 @@
 package Logalyzer;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Created by rhenderson on 6/28/2016.
  */
 public class Logalyzer {
+
+    private String logPath;
+
+    public static void main(String args[]){
+        if(args.length == 0){
+            GUI graphicalInterface = new GUI("BlackBerry IVR Log Analyzer", 1024, 768);
+            try{
+                //graphicalInterface.getDisplayConsole().append(searchJavaSession("59BF2C55C1440A8FEEEBB1538D9D6064:/bbIVR").toString());
+                System.out.println("Running command");
+                System.out.println("output: " + searchJavaSession("59BF2C55C1440A8FEEEBB1538D9D6064:/bbIVR").toString());
+            }
+            catch(LogException e){
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+        else{
+            return;
+        }
+
+    }
+
+    private static StringBuilder searchJavaSession(String sessionId)throws LogException{
+        try{
+            StringBuilder output = new StringBuilder();
+            String line;
+            Process p = Runtime.getRuntime().exec("c:\\cygwin64\\bin\\grep.exe " + "-e \"sessionId\"" + " \"c:\\rorydev\\telephony\\log traces\\goodcalls\\firstCall.txt");
+            BufferedReader bri = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while((line = bri.readLine()) != null){
+                output.append(line);
+            }
+            bri.close();
+
+            /*if(output == null)
+            {
+                BufferedReader bre = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+                while((line = bre.readLine()) != null){
+                    output.append(line);
+                }
+                bre.close();
+            }*/
+
+            p.waitFor();
+            return output;
+        }
+        catch(Exception e){
+            throw new LogException("Error occurred: " + e.getMessage());
+        }
+
+
+    }
 }
